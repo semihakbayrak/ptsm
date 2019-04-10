02b109c835963d407a710d8254001576b4058135
semihakbayrak <semihakbayrak@gmail.com>
2019-04-10 16:02:30 +0200

    LDS refactor

--- LDS/lds.py
+++ LDS/lds.py
@@ -59,24 +59,39 @@ class LDS:
 			observations.append(observation)
 		return states,observations
 
+	#methods for filtering and smoothing
+	def filter_one_step(self, y_t, A_t, B_t, E_h_t, E_o_t, f_t_old, F_t_old):
+		m_h = np.dot(A_t,f_t_old)
+		m_o = np.dot(B_t,m_h)
+		E_hh = np.dot(A_t,np.dot(F_t_old,A_t.T)) + E_h_t
+		E_oo = np.dot(B_t,np.dot(E_hh,B_t.T)) + E_o_t
+		E_oh = np.dot(B_t,E_hh)
+		E_ho = E_oh.T
+		f_t = m_h + np.dot(np.dot(E_ho,np.linalg.inv(E_oo+np.eye(self.O)*10**-8)),(y_t.reshape(self.O,1)-m_o))
+		F_t = E_hh - np.dot(np.dot(E_ho,np.linalg.inv(E_oo+np.eye(self.O)*10**-8)),E_oh)
+		return f_t, F_t
+
+	def smooth_one_step(self, A_t_plus_1, E_h_t_plus_1, f_t, F_t, h_t_plus_1, H_t_plus_1):
+		P1 = F_t
+		P21 = np.dot(A_t_plus_1,F_t)
+		P12 = P21.T
+		P2 = np.dot(P21,A_t_plus_1.T) + E_h_t_plus_1
+		P2inv = np.linalg.inv(P2+np.eye(self.S)*10**-8)
+		h_t = f_t - np.dot(np.dot(P12,P2inv),np.dot(A_t_plus_1,f_t)) + np.dot(np.dot(P12,P2inv),h_t_plus_1)
+		H_t = np.dot(np.dot(P12,np.dot(P2inv,H_t_plus_1)),np.dot(P2inv.T,P21)) + P1 - np.dot(np.dot(P12,P2inv),P21)
+		return h_t, H_t
+
 	#Inference-Filtering
 	def filtering(self,y,f=0,F=0,f_list=[],F_list=[],count=0):
 		if type(f) == int:
 			if f == 0:
 				if self.S>1:
-					f=np.zeros(self.S).reshape(self.S,1)
+					f = np.zeros(self.S).reshape(self.S,1)
 		if type(F) == int:
 			if F == 0:
 				if self.S>1:
-					np.zeros((self.S,self.S))
-		m_h = np.dot(self.A,f)
-		m_o = np.dot(self.B,m_h)
-		E_hh = np.dot(self.A,np.dot(F,self.A.T)) + self.E_h
-		E_oo = np.dot(self.B,np.dot(E_hh,self.B.T)) + self.E_o
-		E_oh = np.dot(self.B,E_hh)
-		E_ho = E_oh.T
-		f_new = m_h + np.dot(np.dot(E_ho,np.linalg.inv(E_oo)),(y[count].reshape(self.O,1)-m_o))
-		F_new = E_hh - np.dot(np.dot(E_ho,np.linalg.inv(E_oo)),E_oh)
+					F = np.zeros((self.S,self.S))
+		f_new, F_new = self.filter_one_step(y[count],self.A,self.B,self.E_h,self.E_o,f,F)
 		f_list.append(f_new)
 		F_list.append(F_new)
 		count = count + 1
@@ -145,13 +160,7 @@ class LDS:
 		elif count < len(y):
 			f = f_list[self.K-1-count]
 			F = F_list[self.K-1-count]
-			P1 = F
-			P21 = np.dot(self.A,F)
-			P12 = P21.T
-			P2 = np.dot(P21,self.A.T) + self.E_h
-			P2inv = np.linalg.inv(P2)
-			h_new = f - np.dot(np.dot(P12,P2inv),np.dot(self.A,f)) + np.dot(np.dot(P12,P2inv),h)
-			H_new = np.dot(np.dot(P12,np.dot(P2inv,H)),np.dot(P2inv.T,P21)) + P1 - np.dot(np.dot(P12,P2inv),P21)
+			h_new, H_new = self.smooth_one_step(self.A,self.E_h,f,F,h,H)
 			h_list.append(h_new)
 			H_list.append(H_new)
 			count = count + 1
@@ -161,53 +170,69 @@ class LDS:
 			return h_list.reverse(), H_list.reverse()
 
 	#Parameter estimation with EM
-	def EM(self,y,A_init=np.nan,B_init=np.nan,pi_m_init=np.nan,pi_s_init=np.nan,E_h_init=np.nan,E_o_init=np.nan,num_iterations=5):
+	def EM(self,y,A_init=np.nan,B_init=np.nan,pi_m_init=np.nan,pi_s_init=np.nan,E_h_init=np.nan,E_o_init=np.nan,estimate=['A','B','pi_m','pi_s','E_h','E_o'],num_iterations=5):
+		#parameters to be estimated
+		A_est = True if 'A' in estimate else False
+		B_est = True if 'B' in estimate else False
+		pi_m_est = True if 'pi_m' in estimate else False
+		pi_s_est = True if 'pi_s' in estimate else False
+		E_h_est = True if 'E_h' in estimate else False
+		E_o_est = True if 'E_o' in estimate else False
+		#initialization
 		if np.isnan(A_init):
-			self.A = np.random.rand(self.S,self.S)
+			self.A = np.random.rand(self.S,self.S) if A_est == True	else self.A	
 		if np.isnan(B_init):
-			self.B = np.random.rand(self.O,self.S)
+			self.B = np.random.rand(self.O,self.S) if B_est == True else self.B
 		if np.isnan(pi_m_init):
-			self.pi_m = np.random.rand(self.S)
+			self.pi_m = np.random.rand(self.S) if pi_m_est == True else self.pi_m
 		if np.isnan(pi_s_init):
-			self.pi_s = np.eye(self.S)
+			self.pi_s = np.eye(self.S) if pi_s_est == True else self.pi_s
 		if np.isnan(E_h_init):
-			self.E_h = np.eye(self.S)
+			self.E_h = np.eye(self.S) if E_h_est == True else self.E_h
 		if np.isnan(E_o_init):
-			self.E_o = np.eye(self.O)
+			self.E_o = np.eye(self.O) if E_o_est == True else self.E_o
 		#EM iterations
 		for _ in range(num_iterations):
 			#E-step
 			h_list, H_list = self.smoothing(y)
 			f_list, F_list = self.filtering(y)
 			#M-step
-			self.pi_m = h_list[0]
-			self.pi_s = H_list[0]
+			self.pi_m = h_list[0] if pi_m_est == True else self.pi_m #pi_m update
+			self.pi_s = H_list[0] if pi_s_est == True else self.pi_s #pi_s update
+			#A update
+			if A_est == True:
+				sum1, sum2 = 0, 0
+				for i in range(self.K-1):
+					t = i + 1
+					P2 = np.dot(self.A,np.dot(F_list[t-1],self.A.T)) + self.E_h
+					P21 = np.dot(self.A,F_list[t-1])
+					sum1 += np.dot(H_list[t].T, np.dot(np.linalg.inv(P2.T+np.eye(self.S)*10**-5),P21)) + np.dot(h_list[t], h_list[t-1].T)
+					sum2 += H_list[t-1] + np.dot(h_list[t-1],h_list[t-1].T)
+				self.A = np.dot(sum1, np.linalg.inv(sum2+np.eye(self.S)*10**-5))
+			#B update
 			sum1, sum2 = 0, 0
-			for i in range(self.K-1):
-				t = i + 1
-				P2 = np.dot(self.A,np.dot(F_list[t-1],self.A.T)) + self.E_h
-				P21 = np.dot(self.A,F_list[t-1])
-				sum1 += np.dot(H_list[t].T, np.dot(np.linalg.inv(P2.T),P21)) + np.dot(h_list[t], h_list[t-1].T)
-				sum2 += H_list[t-1] + np.dot(h_list[t-1],h_list[t-1].T)
-			self.A = np.dot(sum1, np.linalg.inv(sum2))
-			sum1 = 0
 			for t in range(self.K):
 				sum1 += np.dot(y[t].reshape(self.O,1), h_list[t].T)
-			sum2 += H_list[self.K-1] + np.dot(h_list[self.K-1],h_list[self.K-1].T)
-			self.B = np.dot(sum1, np.linalg.inv(sum2))
+				sum2 += H_list[t] + np.dot(h_list[t],h_list[t].T)
+			self.B = np.dot(sum1, np.linalg.inv(sum2+np.eye(self.S)*10**-5)) if B_est == True else self.B
+			#E_o update
 			sum1 = np.dot(sum1, self.B.T)
 			sum3 = 0
-			for t in range(self.K):
-				sum3 += np.dot(y[t].reshape(self.O,1), y[t].reshape(1,self.O))
-			self.E_o = (sum3 - sum1)/self.K
-			sum2 = sum2 - H_list[0] - np.dot(h_list[0],h_list[0].T)
-			sum1 = 0
-			for i in range(self.K-1):
-				t = i + 1
-				P2 = np.dot(self.A,np.dot(F_list[t-1],self.A.T)) + self.E_h
-				P21 = np.dot(self.A,F_list[t-1])
-				sum1 += (np.dot(H_list[t].T, np.dot(np.linalg.inv(P2.T),P21)) + np.dot(h_list[t], h_list[t-1].T)).T
-			sum1 = np.dot(self.A, sum1)
-			self.E_h = (sum2 - sum1)/(self.K-1)
+			if E_o_est == True:
+				for t in range(self.K):
+					sum3 += np.dot(y[t].reshape(self.O,1), y[t].reshape(1,self.O))
+				self.E_o = (sum3 - sum1)/self.K
+			#E_h update
+			if E_h_est == True:
+				sum2 = sum2 - H_list[0] - np.dot(h_list[0],h_list[0].T)
+				sum1 = 0
+				for i in range(self.K-1):
+					t = i + 1
+					P2 = np.dot(self.A,np.dot(F_list[t-1],self.A.T)) + self.E_h
+					P21 = np.dot(self.A,F_list[t-1])
+					sum1 += (np.dot(H_list[t].T, np.dot(np.linalg.inv(P2.T+np.eye(self.S)*10**-5),P21)) + np.dot(h_list[t], h_list[t-1].T)).T
+				sum1 = np.dot(self.A, sum1)
+				self.E_h = (sum2 - sum1)/(self.K-1)
+		#Run and return the smoothing one more time after parameters are estimated
 		h_list, H_list = self.smoothing(y)
 		return self.A, self.B, self.pi_m, self.pi_s, self.E_h, self.E_o, h_list, H_list
